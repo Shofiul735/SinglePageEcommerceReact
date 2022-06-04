6b564a59fcd4b793a51e99a78d5f58bafc6b7456
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-04 21:40:14 +0600

    enhancement #1: Gallery section completed
    Gallery controll bar added in the gallery section and click listener added in the gallery controll bar

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,16 +1,42 @@
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
+import GalleryControlBar from "./GalleryControlBar";
+import { replaceDisplayImage } from "../../store/gallery";
 
 import classes from "./Gallery.module.scss";
 
 const Gallery = (props) => {
     const dispatch = useDispatch();
-    const imageLink = useSelector(state => state.gallery.displayImageLink);
-    const images = useSelector(state => state.gallery.images);
+    const imageLink = useSelector((state) => state.gallery.displayImageLink);
+    const images = useSelector((state) => state.gallery.images);
+
+    const displayImageChangeHandler = (index) => {
+        dispatch(replaceDisplayImage(index));
+    };
+
+    const galleryControlBar = images.map((item, index) => {
+        return (
+            <GalleryControlBar
+                key={index}
+                src={item.thumb}
+                click={() => displayImageChangeHandler(index)}
+            />
+        );
+    });
 
     return (
         <div className={classes.gallery}>
-            <div className={classes['image-section']}>
-                <img src={imageLink} alt="product-image"/>
+            <div className={classes["image-section"]}>
+                <img
+                    className={classes["display-image"]}
+                    src={imageLink}
+                    alt="product-image"
+                />
+                <div className={classes['gallery-control-section']}>
+                    <div className={classes["gallery-controller"]}>
+                        {galleryControlBar}
+                    </div>
+                </div>
             </div>
         </div>
     );
