b07fc92fb66bdd8428eb58f685d16ca216e01814
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-04 20:48:29 +0600

    #1: Gallery display image added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,18 @@
-import classes from './Gallery.module.scss';
+import { useDispatch,useSelector } from "react-redux";
+
+import classes from "./Gallery.module.scss";
 
 const Gallery = (props) => {
-    return <div className={classes.gallery}>
+    const dispatch = useDispatch();
+    const imageLink = useSelector(state => state.gallery.displayImageLink);
+    const images = useSelector(state => state.gallery.images);
 
-    </div>;
-}
+    return (
+        <div className={classes.gallery}>
+            <div className={classes['image-section']}>
+                <img src={imageLink} alt="product-image"/>
+            </div>
+        </div>
+    );
+};
 export default Gallery;
