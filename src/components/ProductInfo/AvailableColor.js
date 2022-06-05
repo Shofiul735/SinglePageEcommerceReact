f1a9a87bb3ff2da9a4b02d0b7f2f28acf9bead15
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 21:33:44 +0600

    #4: color selecction and disable functionality implemented

--- src/components/ProductInfo/AvailableColor.js
+++ src/components/ProductInfo/AvailableColor.js
@@ -1,8 +1,14 @@
 import classes from './AvailableColor.module.scss';
 
 const AvailableColor = (props) => {
+    const className = [classes['color-button']];
+
+    if(props.active){
+        className.push(classes['active'])
+    }
+    
     return (
-        <button className={classes['color-button']} onClick={props.onClick}>
+        <button disabled={!props.isAvailable} className={className.join(' ')} onClick={props.onClick}>
             <img src={props.src} className={classes['color-img']} alt="thumb"/>
         </button>
     );
