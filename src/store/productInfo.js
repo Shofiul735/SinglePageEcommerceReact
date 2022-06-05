552380923b076a12c74c2b2f4719152a16b2d374
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 17:38:06 +0600

    #3: checkAvailability function implemented

--- src/store/productInfo.js
+++ src/store/productInfo.js
@@ -16,6 +16,17 @@ const initialState = {
     },
 }
 
+const checkAvailability= (indicator,id,skus) => {
+    const result= {};
+    result.indicator = indicator;
+    if(indicator === 'size'){
+        result.avaiable = skus.map(item => item.props[0] == id);
+    }else{
+        result.avaiable = skus.map(item => item.props[1] == id);
+    }
+    return result;
+}
+
 const slice = createSlice({
     name: 'ProductInfo',
     initialState,
