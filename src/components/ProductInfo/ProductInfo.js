79c74e190a4e64026db0fda050cd219304e7d561
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 19:22:13 +0600

    #4 price section implemented
    In the price section current price, old price and total discunt displayed

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -5,11 +5,19 @@ import classes from './ProductInfo.module.scss';
 const ProductInfo = (props) => {
 
     let title = useSelector((state)=>state.product.title);
+    const price = useSelector((state)=>state.product.price);
+
+
     title = title.split('&#39;').join("'");
 
     return <div className={classes['product-info']}>
         <p className={classes['title']}>{title}</p>
-        
+        <div className={classes['price-section']}>
+            <h4>Price:</h4>
+            <h2 className={classes['new-price']}> Rs.{price.new}</h2>
+            <h2 className={classes['old-price']}> Rs.{price.old}</h2>
+            <h2 className={classes.discount}> ({price.totalDiscount.toFixed(2)}% OFF)</h2>
+        </div>
     </div>
 }
 
