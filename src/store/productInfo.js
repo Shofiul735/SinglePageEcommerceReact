6d331e9b99d1d022e00089a722aa26b733b5479e
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 17:08:59 +0600

    #3: initalState created
    initialState consist of product title, price, colors, sizes, skus, and its availability

---
+++ src/store/productInfo.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    title:'',
+    price:{
+        new: 0,
+        old: 0,
+        totalDiscount: 0,
+    },
+    colors: [],
+    sizes: [],
+    skus: [],
+    availability:{
+        indicator: 'size',
+        avaiable: [],
+    },
+}
+
