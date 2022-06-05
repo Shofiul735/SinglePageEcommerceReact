72f5834cabf24dbe4d0f8a47fea9b845561708d7
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 17:54:22 +0600

    #3: productSlice registered in the redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import galleryReducer from "./gallery";
+import productReducer from './productInfo';
 
 const store = configureStore({
     reducer: {
         gallery: galleryReducer, 
+        product: productReducer,
     },
 });
 
--- src/store/productInfo.js
+++ src/store/productInfo.js
@@ -41,3 +41,6 @@ const slice = createSlice({
         }
     }
 });
+
+export const {populateProductInfo} = slice.actions;
+export default slice.reducer;
