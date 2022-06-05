ce013e2241428589825eea8f83e21bbbd1e67485
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 21:43:08 +0600

    #4: AvailableColor component added and dispatch implemented in the ProductInfo component

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,41 +1,82 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import AvailableColor from './AvailableColor';
-import classes from './ProductInfo.module.scss';
+import AvailableColorButton from "./AvailableColor";
+
+import { replaceDisplayImage } from "../../store/gallery";
+import { updateSelectedColor } from "../../store/productInfo";
+
+import classes from "./ProductInfo.module.scss";
 
 const ProductInfo = (props) => {
+    const dispatch = useDispatch();
 
     let title = useSelector((state) => state.product.title);
     const price = useSelector((state) => state.product.price);
     const colors = useSelector((state) => state.product.colors);
     const availability = useSelector((state) => state.product.availability);
+    const selectedColor = useSelector((state) => state.product.selectedColor);
 
-    title = title.split('&#39;').join("'");
-    console.log(colors);
-    const colorsButton = colors.map((item,index)=>{
-        if(availability.indicator === 'color'){
-            return <AvailableColor isAvailable={availability.avaiable[index]} src={item.thumb}/>
-        }else{
-            return <AvailableColor isAvailable={true} src={item.thumb}/>
+    title = title.split("&#39;").join("'");
+
+    const colorHandler = (index) => {
+        const newColor = {
+            name: colors[index].name,
+            index: index,
+        };
+
+        dispatch(
+            replaceDisplayImage({
+                type: "color",
+                url: colors[index].image,
+            })
+        );
+
+        dispatch(updateSelectedColor(newColor));
+    };
+
+    const colorsButton = colors.map((item, index) => {
+        if (availability.indicator === "color") {
+            return (
+                <AvailableColorButton
+                    active={selectedColor.index === index}
+                    key={index}
+                    isAvailable={availability.avaiable[index]}
+                    src={item.thumb}
+                    onClick={() => colorHandler(index)}
+                />
+            );
+        } else {
+            return (
+                <AvailableColorButton
+                    active={selectedColor.index === index}
+                    key={index}
+                    isAvailable={true}
+                    src={item.thumb}
+                    onClick={() => colorHandler(index)}
+                />
+            );
         }
-    })
+    });
 
-
-    return <div className={classes['product-info']}>
-        <p className={classes['title']}>{title}</p>
-        <div className={classes['price-section']}>
-            <h4>Price:</h4>
-            <h2 className={classes['new-price']}> Rs.{price.new}</h2>
-            <h2 className={classes['old-price']}> Rs.{price.old}</h2>
-            <h2 className={classes.discount}> ({price.totalDiscount.toFixed(2)}% OFF)</h2>
-        </div>
-        <div className={classes['color-section']}>
-            <p>Color: <span>Select a color</span></p>
-            <div className={classes['color-variety']}>
-                {colorsButton}
+    return (
+        <div className={classes["product-info"]}>
+            <p className={classes["title"]}>{title}</p>
+            <div className={classes["price-section"]}>
+                <h4>Price:</h4>
+                <h2 className={classes["new-price"]}> Rs.{price.new}</h2>
+                <h2 className={classes["old-price"]}> Rs.{price.old}</h2>
+                <h2 className={classes.discount}>
+                    ({price.totalDiscount.toFixed(2)}% OFF)
+                </h2>
+            </div>
+            <div className={classes["color-section"]}>
+                <p>
+                    Color: <span>{selectedColor.name}</span>
+                </p>
+                <div className={classes["color-variety"]}>{colorsButton}</div>
             </div>
         </div>
-    </div>
-}
+    );
+};
 
 export default ProductInfo;
