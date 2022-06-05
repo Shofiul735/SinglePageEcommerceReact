6aeab643a629a136dd536533c189bb8ae58fd2d5
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 20:13:08 +0600

    enhancement #4: color button added

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,14 +1,25 @@
 import { useSelector } from 'react-redux';
 
+import AvailableColor from './AvailableColor';
 import classes from './ProductInfo.module.scss';
 
 const ProductInfo = (props) => {
 
-    let title = useSelector((state)=>state.product.title);
-    const price = useSelector((state)=>state.product.price);
-
+    let title = useSelector((state) => state.product.title);
+    const price = useSelector((state) => state.product.price);
+    const colors = useSelector((state) => state.product.colors);
+    const availability = useSelector((state) => state.product.availability);
 
     title = title.split('&#39;').join("'");
+    console.log(colors);
+    const colorsButton = colors.map((item,index)=>{
+        if(availability.indicator === 'color'){
+            return <AvailableColor isAvailable={availability.avaiable[index]} src={item.thumb}/>
+        }else{
+            return <AvailableColor isAvailable={true} src={item.thumb}/>
+        }
+    })
+
 
     return <div className={classes['product-info']}>
         <p className={classes['title']}>{title}</p>
@@ -19,9 +30,9 @@ const ProductInfo = (props) => {
             <h2 className={classes.discount}> ({price.totalDiscount.toFixed(2)}% OFF)</h2>
         </div>
         <div className={classes['color-section']}>
-            <p>Color: <span>Balck</span></p>
+            <p>Color: <span>Select a color</span></p>
             <div className={classes['color-variety']}>
-
+                {colorsButton}
             </div>
         </div>
     </div>
