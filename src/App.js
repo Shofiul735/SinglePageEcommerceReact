5de17e914c5011fb0fcd28cdddf341d28bb2090c
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-05 18:08:54 +0600

    #4: productInfo added in the redux store

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import { Fragment, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { populateData } from "./store/gallery";
+import { populateProductInfo } from "./store/productInfo";
 
 import Header from "./components/Header/Header";
-import MainPage from './components/MainPage/MainPage';
+import MainPage from "./components/MainPage/MainPage";
 
 const App = (props) => {
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
         fetch(
             "https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product"
         )
             .then((response) => response.json())
             .then((data) => {
-                dispatch(populateData({
+                const productData = {
+                    title: data.title,
+                    price: {
+                        new: data.price.discounted,
+                        old: data.price.old,
+                        totalDiscount:
+                            (Math.abs(data.price.discounted - data.price.old) /
+                                data.price.old) *
+                            100,
+                    },
+                    colors: data.variation.props[0].values,
+                    sizes: data.variation.props[1].values,
+                    skus: data.variation.skus,
+                };
+
+                const galleryData = {
                     images: data.gallery,
                     displayLink: data.gallery[0].url,
-                }));
+                };
+
+                dispatch(populateProductInfo(productData));
+                dispatch(populateData(galleryData));
                 console.log("App.js--21");
+                console.log(data);
             });
-    },[]);
-    
+    }, []);
 
     return (
         <Fragment>
