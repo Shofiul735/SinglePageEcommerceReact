7b19b72e5485d57cea13f68b24e9e8fb19b85b57
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 14:21:25 +0600

    #4: Price section moved to a new folder
    Price section moved to a new folder named PriceSection

---
+++ src/components/PriceSection/PriceSection..js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+
+import classes from './PriceSection.module.scss';
+
+const PriceSection = (props) => {
+    const price = useSelector((state) => state.product.price);
+
+    return (
+        <div className={classes["price-section"]}>
+            <h4>Price:</h4>
+            <h2 className={classes["new-price"]}> Rs.{price.new}</h2>
+            <h2 className={classes["old-price"]}> Rs.{price.old}</h2>
+            <h2 className={classes.discount}>
+                ({price.totalDiscount.toFixed(2)}% OFF)
+            </h2>
+        </div>
+    );
+};
+
+export default PriceSection;
