a85ca415052df99406e0d313da82735e3ab08916
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 14:15:37 +0600

    #4: AvailableColors component added in the ProdunctInfo component and renamed the file

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,62 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import AvailableColorButton from "./AvailableColor";
-
-import { replaceDisplayImage } from "../../store/gallery";
-import { updateSelectedColor } from "../../store/productInfo";
+import AvailableColors from "../AvailableColor/AvailableColors";
 
 import classes from "./ProductInfo.module.scss";
 
 const ProductInfo = (props) => {
-    const dispatch = useDispatch();
-
     let title = useSelector((state) => state.product.title);
     const price = useSelector((state) => state.product.price);
-    const colors = useSelector((state) => state.product.colors);
-    const availability = useSelector((state) => state.product.availability);
-    const selectedColor = useSelector((state) => state.product.selectedColor);
 
     title = title.split("&#39;").join("'");
 
-    const colorHandler = (index) => {
-        const newColor = {
-            name: colors[index].name,
-            index: index,
-        };
-
-        dispatch(
-            replaceDisplayImage({
-                type: "color",
-                url: colors[index].image,
-            })
-        );
-
-        dispatch(updateSelectedColor(newColor));
-    };
-
-    const colorsButton = colors.map((item, index) => {
-        if (availability.indicator === "color") {
-            return (
-                <AvailableColorButton
-                    active={selectedColor.index === index}
-                    key={index}
-                    isAvailable={availability.avaiable[index]}
-                    src={item.thumb}
-                    onClick={() => colorHandler(index)}
-                />
-            );
-        } else {
-            return (
-                <AvailableColorButton
-                    active={selectedColor.index === index}
-                    key={index}
-                    isAvailable={true}
-                    src={item.thumb}
-                    onClick={() => colorHandler(index)}
-                />
-            );
-        }
-    });
 
     return (
         <div className={classes["product-info"]}>
@@ -69,12 +22,7 @@ const ProductInfo = (props) => {
                     ({price.totalDiscount.toFixed(2)}% OFF)
                 </h2>
             </div>
-            <div className={classes["color-section"]}>
-                <p>
-                    Color: <span>{selectedColor.name}</span>
-                </p>
-                <div className={classes["color-variety"]}>{colorsButton}</div>
-            </div>
+            <AvailableColors />
         </div>
     );
 };
