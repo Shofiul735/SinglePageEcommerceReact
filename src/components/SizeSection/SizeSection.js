d14ce8351d303ef47576b6f2edf833168bd273e2
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 14:49:34 +0600

    #4: Basic layout implemented for SizeSection

--- src/components/SizeSection/SizeSection.js
+++ src/components/SizeSection/SizeSection.js
@@ -1,7 +1,21 @@
-import classes from './SizeSection.module.scss';
+import { useSelector } from "react-redux";
+
+import classes from "./SizeSection.module.scss";
 
 const SizeSection = (props) => {
+    const sizes = useSelector((state) => state.product.sizes);
+    const availability = useSelector((state) => state.product.availability);
+    console.log(sizes);
+    console.log(availability);
 
-}
+    return (
+        <div className={classes["size-section"]}>
+            <p>
+                Size: <span>{5}</span>
+            </p>
+            <div className={classes["size-variety"]}>Hello</div>
+        </div>
+    );
+};
 
 export default SizeSection;
