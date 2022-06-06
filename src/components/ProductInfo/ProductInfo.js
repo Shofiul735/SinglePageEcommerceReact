b8ff1de387063b98185da23cad3a17e0b59c27f0
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 14:24:49 +0600

    #4: useDispatch funtion removed

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import AvailableColors from "../AvailableColor/AvailableColors";
 import PriceSection from "../PriceSection/PriceSection.";
