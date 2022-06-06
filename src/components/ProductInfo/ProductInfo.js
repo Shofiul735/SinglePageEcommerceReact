42745d458f6a9590e21967228559801a75d8ef4d
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 14:22:03 +0600

    #4: PriceSection added in the ProductInfo component

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -1,12 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import AvailableColors from "../AvailableColor/AvailableColors";
+import PriceSection from "../PriceSection/PriceSection.";
 
 import classes from "./ProductInfo.module.scss";
 
 const ProductInfo = (props) => {
     let title = useSelector((state) => state.product.title);
-    const price = useSelector((state) => state.product.price);
 
     title = title.split("&#39;").join("'");
 
@@ -14,14 +14,7 @@ const ProductInfo = (props) => {
     return (
         <div className={classes["product-info"]}>
             <p className={classes["title"]}>{title}</p>
-            <div className={classes["price-section"]}>
-                <h4>Price:</h4>
-                <h2 className={classes["new-price"]}> Rs.{price.new}</h2>
-                <h2 className={classes["old-price"]}> Rs.{price.old}</h2>
-                <h2 className={classes.discount}>
-                    ({price.totalDiscount.toFixed(2)}% OFF)
-                </h2>
-            </div>
+            <PriceSection />
             <AvailableColors />
         </div>
     );
