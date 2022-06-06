90892289375ddee8755f064125902a680289d28b
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 16:34:16 +0600

    #4: active class added in the button component

--- src/components/SizeSection/SizeButton.js
+++ src/components/SizeSection/SizeButton.js
@@ -1,7 +1,12 @@
 import classes from './SizeButton.module.scss';
 
 const SizeButton = (props) => {
-    return <button className={classes['size-button']} onClick={props.onClick} disabled={props.disabled}>
+    const classNames = [classes['size-button']];
+
+    if(props.active){
+        classNames.push(classes['active']);
+    }
+    return <button className={classNames.join(' ')} onClick={props.onClick} disabled={props.disabled}>
         {props.size}
     </button>
 }
