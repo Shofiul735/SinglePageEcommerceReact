74bd78f943ea4885d050ba9367bbc499fabcc864
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 16:00:42 +0600

    SizeButton implemented

--- src/components/SizeSection/SizeButton.js
+++ src/components/SizeSection/SizeButton.js
@@ -1,6 +1,8 @@
 import classes from './SizeButton.module.scss';
 
 const SizeButton = (props) => {
-
+    return <button className={classes['size-button']} onClick={props.onClick} disabled={props.disabled}>
+        {props.size}
+    </button>
 }
 export default SizeButton;
