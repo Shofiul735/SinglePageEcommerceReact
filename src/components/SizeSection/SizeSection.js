64bb0090bc446d04bc2d1da854b91faede63cabe
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 16:23:59 +0600

    #4: sizeSelectionHandler implemented
     This function will update the product price and and selectedSize details

--- src/components/SizeSection/SizeSection.js
+++ src/components/SizeSection/SizeSection.js
@@ -1,19 +1,42 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 
 import SizeButton from "./SizeButton";
+
+import { updateSelectedSize, updatePrice } from "../../store/productInfo";
 
 import classes from "./SizeSection.module.scss";
 
 const SizeSection = (props) => {
+    const dispatch = useDispatch();
     const sizes = useSelector((state) => state.product.sizes);
     const availability = useSelector((state) => state.product.availability);
     const skus = useSelector((state) => state.product.skus);
+    const colorId = useSelector((state) => state.product.selectedColor.id);
 
-    const sizeButtons = sizes.map((item) => {
+    const sizeSelectionHandler = (index) => {
+        const selectedSize = {
+            id: sizes[index].id,
+            name: sizes[index].name,
+            index: index,
+        };
+        const newPrice = skus.find(
+            (item) =>
+                item.props[0] === colorId && item.props[1] === selectedSize.id
+        );
+        const displayPrice = {
+            new: newPrice.price.discounted,
+            old: newPrice.price.old,
+        }
+        dispatch(updateSelectedSize(selectedSize));
+        dispatch(updatePrice(displayPrice));
+    };
+
+    const sizeButtons = sizes.map((item, index) => {
         return (
             <SizeButton
-                key={item.id}
+                key={index}
                 size={item.name}
+                onClick={() => sizeSelectionHandler(index)}
                 disabled={availability[item.id] === true ? false : true}
             />
         );
