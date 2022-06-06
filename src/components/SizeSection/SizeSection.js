aad98bf6de57a17f676ffa79be0072bcda848dd7
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 15:51:07 +0600

    #4: SizeButton added in the SizeSection component

--- src/components/SizeSection/SizeSection.js
+++ src/components/SizeSection/SizeSection.js
@@ -1,19 +1,30 @@
 import { useSelector } from "react-redux";
 
+import SizeButton from "./SizeButton";
+
 import classes from "./SizeSection.module.scss";
 
 const SizeSection = (props) => {
     const sizes = useSelector((state) => state.product.sizes);
     const availability = useSelector((state) => state.product.availability);
-    console.log(sizes);
-    console.log(availability);
+    const skus = useSelector((state) => state.product.skus);
+
+    const sizeButtons = sizes.map((item) => {
+        return (
+            <SizeButton
+                key={item.id}
+                size={item.name}
+                disabled={availability[item.id] === true ? false : true}
+            />
+        );
+    });
 
     return (
         <div className={classes["size-section"]}>
             <p>
                 Size: <span>{5}</span>
             </p>
-            <div className={classes["size-variety"]}>Hello</div>
+            <div className={classes["size-variety"]}>{sizeButtons}</div>
         </div>
     );
 };
