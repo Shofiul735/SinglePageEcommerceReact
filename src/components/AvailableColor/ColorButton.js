d40d76889a096f94963282f690cb610331a69038
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-06-06 13:53:52 +0600

    #4: separate button created for color section

---
+++ src/components/AvailableColor/ColorButton.js
@@ -0,0 +1,16 @@
+import classes from './ColorButton.module.scss';
+
+const AvailableColor = (props) => {
+    const className = [classes['color-button']];
+
+    if(props.active){
+        className.push(classes['active'])
+    }
+    
+    return (
+        <button disabled={!props.isAvailable} className={className.join(' ')} onClick={props.onClick}>
+            <img src={props.src} className={classes['color-img']} alt="thumb"/>
+        </button>
+    );
+}
+export default AvailableColor;
